c2c2e00c18487777beb4cc92389fca6de3f2ff52
Logan <logan.lucas83@gmail.com>
2022-04-25 16:47:20 +0200

    Ajout de programmes déjà avancés

---
+++ complex.py
@@ -0,0 +1,151 @@
+import math
+
+class Complex():
+    acc = 3
+    def __init__(self,real = None,im = None,mod = None,arg = None):
+        if (real is None or im is None) and (mod is None or arg is None):
+            raise TypeError("Needs either real and imaginary part or modulus and argument (mod, arg)")
+        self.real = real if real is not None else  mod * math.cos(arg)
+        self.im = im if im is not None else mod * math.sin(arg)
+        self.modulus = mod if mod is not None else (self.real**2+self.im**2)**0.5
+        if (self.real,self.im) != (0,0):
+            self.argument = arg if arg is not None else math.atan2(self.im,self.real)
+        else:
+            self.argument = 0
+        self.form = 0
+        
+    def __repr__(self):
+        #a = map(lambda x : round(x,Complex.acc),[self.real,self.im,self.modulus,self.argument])
+        if self.form == 0:
+            if self.real and not self.im:
+                return str(self.real)
+            elif self.real and self.im in (1,-1):
+                return f"{round(self.real,Complex.acc)} + i" if self.im > 0 else f"{round(self.real,Complex.acc)} - i"
+            elif self.real:
+                return f"{round(self.real,Complex.acc)} + {abs(round(self.im,Complex.acc))}i" if self.im > 0 else f"{round(self.real,Complex.acc)} - {abs(round(self.im,Complex.acc))}i"
+            elif not self.real and not self.im:
+                return str(self.real)
+            elif not self.real and self.im in (1,-1):
+                return "i" if self.im > 0 else "- i"
+            else:
+                return f"{abs(round(self.im,Complex.acc))}i" if self.im > 0 else f"- {abs(round(self.im,Complex.acc))}i"
+            
+        elif self.form == 1:
+            return f"{round(self.modulus,Complex.acc)}(cos({round(self.argument,Complex.acc)}) + i sin({round(self.argument,Complex.acc)}))"
+
+        else:
+            return f"{round(self.modulus,Complex.acc)}exp({round(self.argument,Complex.acc)}i)"
+            
+    def __add__(self,other):
+        other = cast_to_complex(other)
+        return Complex(self.real+other.real,self.im+other.im)
+
+    def __radd__(self,other):
+        other = cast_to_complex(other)
+        return self+other
+        
+    def __neg__(self):
+        return Complex(-self.real,-self.im)
+
+    def __sub__(self,other):
+        return self+-cast_to_complex(other)
+
+    def __mul__(self,other):
+        other = cast_to_complex(other)
+        return Complex(self.real*other.real-self.im*other.im,self.real*other.im+self.im*other.real)
+
+    def __rmul__(self,other):
+        return self*other
+
+    def conjugate(self):
+        return Complex(self.real,-self.im)
+
+    def __truediv__(self,other):
+        other = cast_to_complex(other)
+        num = self*other.conjugate()
+        den = other.modulus**2
+        return Complex(num.real/den,num.im/den)
+
+    def polar(self):
+        self.form = 1
+        return f"{self.modulus}(cos({self.argument}) + i sin({self.argument}))"
+
+    def expo(self):
+        self.form = 2
+        return f"{self.modulus}exp({self.argument}i)"
+
+    def cartesian(self):
+        return self.modulus * Complex(math.cos(self.argument),math.sin(self.argument))
+
+    def nroot(self,n):
+        roots = [Complex(mod= self.modulus**(1/n),arg= (self.argument+2*k*math.pi)/n) for k in range(n)]
+        return roots
+
+    def __pow__(self,other):
+        output = 1
+        if isinstance(other,int):
+            for i in range(other):
+                output *= self
+            return output
+        if isinstance(other,float):
+            int_part = int(other)
+            decimal = other - int_part
+            output *= self**int_part
+            decimal = Fraction(decimal)
+            decimal.num_to_int()
+            decimal.simplify()
+            return [output*(n**decimal.numerator) for n in self.nroot(decimal.denominator)]
+
+class Fraction:
+    def __init__(self,num,den = 1):
+        self.numerator = num
+        self.denominator = den
+        if self.numerator < 0 and self.denominator < 0:
+            self.numerator = abs(self.numerator)
+            self.denominator = abs(self.denominator)
+
+    def __repr__(self):
+        return f"{self.numerator}/{self.denominator}"
+
+    def _to_int(self):
+        if isinstance(self.numerator,float) and self.numerator.is_integer():
+            self.numerator = int(self.numerator)
+        if isinstance(self.denominator,float) and self.denominator.is_integer():
+            self.denominator = int(self.denominator)
+                    
+    def num_to_int(self):
+        while not self.numerator.is_integer():
+            self.numerator *= 10
+            self.denominator *= 10
+        self._to_int()
+
+    def simplify(self):
+        x = pgdc(self.numerator,self.denominator) 
+        self.numerator /= x
+        self.denominator /= x
+        self._to_int()        
+
+def pgdc(a,b):
+    if b == 0:
+        return a
+    else:
+        return pgdc(b,a%b)
+
+
+def cast_to_complex(truc):
+    if isinstance(truc,Complex):
+        return truc
+    elif isinstance(truc,int) or isinstance(truc,float):
+        return Complex(truc,0)
+
+def factorial(n):
+    output = 1
+    for i in range(2,n+1):
+        output *= i
+    return output
+
+def exp(x,acc = 100):
+    if isinstance(x,Complex):
+        return exp(x.real,acc)*Complex(mod= 1,arg= x.im)
+    else:
+        return sum([(x**i)/factorial(i) for i in range(acc)])
---
+++ matrix.py
@@ -0,0 +1,275 @@
+# Logan LUCAS
+
+import random,copy
+
+class Matrix:
+    def __init__(self,values = None, Isize = 3):
+        if not values:
+            ide = [[1 if i == j else 0 for i in range(Isize)] for j in range(Isize)]
+
+        self.values = values or ide
+        self.width = len(self.values)
+        self.length = len(self.values[0]) if len(self.values) >= 1 else 0
+
+    def __repr__(self):
+        output = ""
+        val = []
+        ldict = {}
+
+        for j in range(self.width):
+            for i in range(self.length):
+                if i not in ldict or len(str(self.values[j][i])) > ldict[i]:
+                    ldict[i] = len(str(self.values[j][i])) 
+                    
+        for i in range(self.length):
+            temp = []
+            for j in range(self.width):
+                dif = ldict[i]-len(str(self.values[j][i]))
+                if dif > 0:
+                    temp.append(" "*dif + str(self.values[j][i])) 
+                else:
+                    temp.append(str(self.values[j][i]))
+            val.append(temp)
+        
+        for i in range(self.width):
+            output+= "( "
+            for j in range(self.length):
+                output+= val[j][i]+" "
+            output+=")\n"
+                
+        return output
+
+    def __add__(self,other):
+        if isinstance(other,Matrix):
+            if self.width == other.width and self.length == other.length:
+                output = []
+                for i in range(self.width):
+                    temp = []
+                    for j in range(self.length):
+                        temp.append(self.values[i][j]+other.values[i][j])
+                    output.append(temp)
+                return Matrix(output)
+
+            print("Matrices don't have the same dimensions")
+
+        print("You can only add matrices together")
+
+    def __radd__(self,other):
+        return self+other
+
+    def __neg__(self):
+        output = []
+        for i in self.values:
+            temp = []
+            for j in i:
+                temp.append(-j)
+            output.append(temp)
+        return Matrix(output)
+
+    def __sub__(self,other):
+        if isinstance(other,Matrix):
+            return -other + self
+
+    def __mul__(self,other):
+        if isinstance(other,int) or isinstance(other,float):
+            output = []
+            for i in self.values:
+                temp = []
+                for j in i:
+                    temp.append(j*other)
+                output.append(temp)
+            return Matrix(output)
+
+        elif isinstance(other,Matrix):
+            if self.length == other.width:
+                output = []
+                for i in range(self.width):
+                    temp = []
+                    for j in range(other.length):
+                        res = 0
+                        for k in range(self.length):
+                            res += self.values[i][k] * other.values[k][j]
+                        temp.append(res)
+                    output.append(temp)
+                return Matrix(output)
+
+            print("Cannot multiply these matrices (nb of columns of the first one != nb of lines of the second one)")
+            
+    def __rmul__(self,other):
+        if isinstance(other,int) or isinstance(other,float):
+            output = []
+            for i in self.values:
+                temp = []
+                for j in i:
+                    temp.append(j*other)
+                output.append(temp)
+            return Matrix(output)
+
+    def __pow__(self,other):
+        if isinstance(other,int):
+            if self.length == self.width:
+                output = Matrix(None,self.length)
+                for i in range(other):
+                    output *= self
+                return output
+            print("Only square matrices can be raised to a power")
+        
+    def __eq__(self,other):
+        if isinstance(other,Matrix) and self.width == other.width and self.length == other.length:
+            for i in range(self.width):
+                for j in range(self.length):
+                    if self.values[i][j] != other.values[i][j]:
+                        return False
+            return True
+        return False
+
+    def __bool__(self):
+        for i in self.values:
+            for j in i:
+                if j != 0:
+                    return True
+        return False
+    __nonzero__ = __bool__
+
+    def __abs__(self):
+        output = []
+        for i in self.values:
+            temp = []
+            for j in i:
+                temp.append(abs(j))
+            output.append(temp)
+        return Matrix(output)
+
+    def __getitem__(self,item):
+        if isinstance(item,int) or isinstance(item,slice):
+            output = []
+            for i in self.values:
+                if isinstance(item,int):
+                    output.append([i[item]])
+                else:
+                    output.append(i[item])
+            return Matrix(output)
+
+    def __setitem__(self,key,value):
+        if isinstance(value,int) and key[0] <= self.width and key[1] <= self.length:
+            self.values[key[0]][key[1]] = value
+        else:
+            print('Error')
+
+    def __delitem__(self,item):
+        if isinstance(item,str):
+            try:
+                todel = int(item[1:])
+                if item[0] == 'l':
+                    del(self.values[todel])
+                    self.width -= 1
+                elif item[0] == 'c':
+                    for i in self.values:
+                        del(i[todel])
+                    self.length -= 1
+                    
+            except ValueError:
+                print("Wrong input")
+            
+    def transpose(self):
+        output = []
+        for i in range(self.length):
+            temp = []
+            for j in range(self.width):
+                temp.append(self.values[j][i])
+            output.append(temp)
+        return Matrix(output)
+
+    def extend(self,other):
+        if isinstance(other,Matrix):
+            if self.width == other.width:
+                output = []
+                for i in range(self.width):
+                    temp = []
+                    temp.extend(self.values[i])
+                    temp.extend(other.values[i])
+                    output.append(temp)
+                return Matrix(output)
+            
+            print("Can only extend matrices with the same nb of lines")
+        print("Can only extend another matrix")
+
+    def determinant(self):
+        if self.width == self.length:
+            if self.width == 2:
+                return self.values[0][0]*self.values[1][1] - self.values[0][1]*self.values[1][0]
+            elif self.width >= 3:
+                output = 0
+                cp = copy.deepcopy(self)
+                del cp['l0']
+                for i in range(self.width):
+                    if i%2 == 0:
+                        signe = 1
+                    else:
+                        signe = -1
+                    coef = self.values[0][i]
+                    deter = cp[0:i].extend(cp[i+1:self.width]).determinant()
+                    output += signe*coef*deter
+                return output 
+
+    def cofactor(self):
+        if self.width == self.length:
+            output = []
+            for i in range(self.width):
+                temp = []
+                cp = copy.deepcopy(self)
+                del cp['l{}'.format(i)]
+                for j in range(self.length):
+                    signe = (-1)**(i+j)
+                    sub = cp[0:j].extend(cp[j+1:self.width])
+                    temp.append(sub.determinant() * signe)
+                output.append(temp)
+            return Matrix(output)
+    
+    def invert(self):
+        deter = self.determinant()
+        if deter:
+            co = self.cofactor()
+            return (1/deter)*co.transpose()
+        print("Matrix isn't invertible")
+
+    def tofract(self):
+        output = []
+        for i in range(self.width):
+            temp = []
+            for j in range(self.length):
+                val = self.values[i][j]
+                temp.append(fract(val))
+            output.append(temp)
+        return Matrix(output)
+
+def fract(fl):
+    output = fl.as_integer_ratio()
+    return "{}/{}".format(output[0],output[1])
+                    
+def newRMatrix(size = 10,value = 1000,isSquare = False):
+    width = random.randrange(1,size)
+    if isSquare:
+        length = width
+    else:
+        length = random.randrange(1,size)
+    output = []
+    for i in range(width):
+        temp = []
+        for j in range(length):
+            temp.append(random.randrange(-value,value))
+        output.append(temp)
+    return Matrix(output)
+
+def newMatrix(value = 1000,width = 3,length = 0):
+    if not length:
+        length = width
+    output = []
+    for i in range(width):
+        temp = []
+        for j in range(length):
+            temp.append(random.randrange(-value,value))
+        output.append(temp)
+    return Matrix(output)
+
+print("newMatrix : value , width , length (all optional)\nnewRMatrix : size , value , isSquare (all optional)\nMatrix(values,Identity size) : identity size only utilized if value is empty")
